dc2d7ed65e8bf209912850975a3c7d266378d916
abraham ghaemi <ig.ghaemi@gmail.com>
2019-08-10 16:32:03 -0400

    related one to many

--- app/Video.php
+++ app/Video.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Video extends Model
 {
     //
+    protected $table = 'video';
+    protected $casts = [
+        'id' => 'string'
+    ];
+
+    public $timestamps = false;
+
+    public function attachment()
+    {
+        return $this->hasOne('App\Attachment', 'related_id', 'id');
+    }
 }
--- routes/console.php
+++ routes/console.php
@@ -9,12 +9,26 @@ Artisan::command('inspire', function () {
 Artisan::command('video:hls', function(){
 	$videos = App\Video::where([
 		'deleted' => 0,
-		'converted' => 0
+        'playlist' => null,
+        'proccess' => 'None'
 	])->get();
 
 	foreach ($videos as $video) {
 		// covnert
-		
+        $video->proccess = 'Start';
+        $video->save();
+        $tmp_upload = '/usr/share/nginx/stream/tmp/' . $video->attachment->name;
+
+        if(file_exists($tmp_upload)){
+            $logo = "/usr/share/nginx/stream/logo.png";
+            $destination = "/usr/share/nginx/stream/repository/" . $video->id . "/";
+            exec("createvod '{$tmp_upload}' '{$logo}' '{$destination}'");
+        }
+
+        $video->vod = "/{$video->id}/backup.mp4";
+        $video->playlist = "/{$video->id}/playlist.m3u8";
+        $video->proccess = 'End';
+        $video->save();
 		// update and save
 	}
 
