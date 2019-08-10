9b0e6bdbff110ec541f583b82102f319b1c24d7e
abraham ghaemi <ig.ghaemi@gmail.com>
2019-08-10 18:19:08 -0400

    update console

--- routes/console.php
+++ routes/console.php
@@ -17,18 +17,27 @@ Artisan::command('video:hls', function(){
 		// covnert
         $video->proccess = 'Start';
         $video->save();
-        $tmp_upload = '/usr/share/nginx/stream/tmp/' . $video->attachment->name;
+        $tmp_upload = "/usr/share/nginx/stream/tmp/" . App\Attachment::find($video->video_file_id)->name;
 
         if(file_exists($tmp_upload)){
-            $logo = "/usr/share/nginx/stream/logo.png";
+            if($video->watermark_id){
+                $logo = "/usr/share/nginx/app/data/upload/{$video->watermark_id}";
+            }else{
+                $logo = "/usr/share/nginx/stream/logo.png";
+            }
             $destination = "/usr/share/nginx/stream/repository/" . $video->id . "/";
             exec("createvod '{$tmp_upload}' '{$logo}' '{$destination}'");
-        }
 
         $video->vod = "/{$video->id}/backup.mp4";
         $video->playlist = "/{$video->id}/playlist.m3u8";
         $video->proccess = 'End';
         $video->save();
+        echo $video->id . " Done. \n\r";
+        } else {
+        echo $video->id . " File not founded. \n\r";
+        
+        }
+
 		// update and save
 	}
 
